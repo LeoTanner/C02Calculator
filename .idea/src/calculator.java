62c0c09fdf1c5878563b165164118a183d41eeba
LeoTanner <s50991@edu.campus02.at>
2021-10-08 19:58:52 +0200

    git commit - variablen deklarieren und output

--- .idea/src/calculator.java
+++ .idea/src/calculator.java
@@ -2,6 +2,16 @@ public class calculator
 {
     public static void main(String[] args)
     {
+        int a = 123;
+        double doublevar = 456;
+        boolean b = true;
+        char symbol = 'c';
+        String name = "Leo Tanner";
 
+        System.out.println("integer: " +a);
+        System.out.println("double: " +doublevar);
+        System.out.println("boolean: "+b);
+        System.out.println("char: "+symbol);
+        System.out.println("String: "+name);
     }
 }
