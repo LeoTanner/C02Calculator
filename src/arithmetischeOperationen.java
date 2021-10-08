283ef24d66f43ee240f5dd33aae6b0e022735a64
LeoTanner <s50991@edu.campus02.at>
2021-10-08 21:15:58 +0200

    git commit - Methoden erklären und einführen

--- src/arithmetischeOperationen.java
+++ src/arithmetischeOperationen.java
@@ -5,6 +5,30 @@ public class arithmetischeOperationen
         int value1 = 4;
         int value2 = 3;
 
+        int t = 3;
+        int r = 2;
+
+        double f = t/ (r*1.0);
+        System.out.println("float f = " + f);
+
+        int o = 3;
+        int l = ++o;
+
+        System.out.println("o = " + o);
+        System.out.println("l = " + l);
+
+        String firstName = "Tanner";
+        String SecondName = "Tanner";
+
+        boolean nametest = firstName.equals(SecondName);
+        System.out.println("boolean nametest = " + nametest);
+
+        boolean b = 2 == 2 && 3 == 3;
+        System.out.println("boolean und = " + b);
+
+        boolean ceh = 2==2||3==4;
+        System.out.println("boolean oder = " + ceh);
+
         //addition
         System.out.println("addition: " + (value1+value2));
 
@@ -19,5 +43,25 @@ public class arithmetischeOperationen
 
         //modulo
         System.out.println("Modulo: "+ value1%value2);
+
+        printResult(2+3);
+        int val123 = 2 + 4;
+        printResult(val123);
+        val123++;
+        printResult(val123);
+
+
+        int result = add (17,3);
+        printResult(result);
+    }
+
+    public static void printResult (int result)
+    {
+        System.out.println("Methodenausgabe: " + result);
+    }
+
+    public static int add (int a, int b)
+    {
+        return a + b;
     }
 }
