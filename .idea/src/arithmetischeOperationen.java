70fde0b97be00ef355c63d2483ea3a955cd478c2
LeoTanner <s50991@edu.campus02.at>
2021-10-08 20:16:07 +0200

    git commit - arithmetischeOperationen

---
+++ .idea/src/arithmetischeOperationen.java
@@ -0,0 +1,23 @@
+public class arithmetischeOperationen
+{
+    public static void main(String[] args)
+    {
+        int value1 = 4;
+        int value2 = 3;
+
+        //addition
+        System.out.println("addition: " +value1+value2);
+
+        //subtraction
+        System.out.println("subtraction: "+ (value1 - value2));
+
+        //multiplikation
+        System.out.println("multiplication: "+ value1*value2);
+
+        //division
+        System.out.println("division: "+ value1/value2);
+
+        //modulo
+        System.out.println("Modulo: "+ value1%value2);
+    }
+}
--- .idea/src/calculator.java
+++ .idea/src/calculator.java
@@ -3,11 +3,17 @@ public class calculator
     public static void main(String[] args)
     {
         int a = 123;
-        double doublevar = 456;
+        double doublevar = 456.789;
         boolean b = true;
         char symbol = 'c';
         String name = "Leo Tanner";
 
+        boolean even = 2 % 2 == 0;
+
+        int e = 2 + 3;
+        int sub = 3 -3;
+
+        System.out.println("even: " + even);
         System.out.println("integer: " +a);
         System.out.println("double: " +doublevar);
         System.out.println("boolean: "+b);
